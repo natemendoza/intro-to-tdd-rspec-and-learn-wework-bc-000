876953f1f5472d07ed8a7c511f64d0a09905fe48
Nathaniel Mendoza <nathaniel.mendoza@wework.com>
2018-02-12 04:57:19 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,8 +1,12 @@
-require_relative './current_age_for_birth_year.rb'
+require_relative '../current_age_for_birth_year.rb'
  
-puts "What year were you born?"
-birth_year = gets.to_i
+describe "current_age_for_birth_year method" do
+  it "returns the age of a person based on the year of birth" do
+    current_year = Time.now.year
+    birth_year = 1984
+    answer = current_year - birth_year
  
-users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
- 
-puts "You are: " + users_age.to_s + " years old."
+    age_of_person = current_age_for_birth_year(birth_year)
+    expect(age_of_person).to eq(answer)
+  end
+end
